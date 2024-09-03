74314a1e38f0c5dc0b66a7df5ee6e87c96df2b31
Driftedboat <keqingc@andrew.cmu.edu>
2024-09-03 15:04:05 -0400

    remove redirect

--- src/middleware/admin.js
+++ src/middleware/admin.js
@@ -35,7 +35,7 @@ middleware.checkPrivileges = helpers.try(async (req, res, next) => {
 			if (noPassword) return;
 			handleReLogin(req, res, next, async (reLoginHandled) => {
 				if (reLoginHandled) return;
-				redirectToLoginIfNeeded(req, res);
+				next();
 			});
 		});
 	});
@@ -96,20 +96,5 @@ function extendLogoutTimer(meta, loginTime, adminReloginDuration) {
 	const timeLeft = parseInt(loginTime, 10) - (Date.now() - adminReloginDuration);
 	if (meta && timeLeft < Math.min(60000, adminReloginDuration)) {
 		meta.datetime += Math.min(60000, adminReloginDuration);
-	}
-}
-function redirectToLoginIfNeeded(req, res) {
-	let returnTo = req.path.replace(/^\/api/, '');
-	if (nconf.get('relative_path')) {
-		returnTo = returnTo.replace(new RegExp(`^${nconf.get('relative_path')}`), '');
-	}
-	req.session.returnTo = returnTo;
-	req.session.forceLogin = 1;
-	plugins.hooks.fire('response:auth.relogin', { req, res });
-	if (res.headersSent) return;
-	if (res.locals.isAPI) {
-		controllers.helpers.formatApiResponse(401, res);
-	} else {
-		res.redirect(`${nconf.get('relative_path')}/login?local=1`);
 	}
 }
